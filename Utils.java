9b37eb6064a28ebe5731dbe4f4ae505b051c87d0
grantpg180 <62108947+grantpg180@users.noreply.github.com>
2020-05-17 23:50:31 -0400

    Add files via upload

---
+++ Utils.java
@@ -0,0 +1,28 @@
+package Labs;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.io.IOException; 
+
+
+
+
+public class Utils {
+    public static void loadStringsToArray(ArrayList<String> arrList) throws IOException {
+        
+        Path path = Paths.get("listOfBooks.txt");
+        arrList.clear();
+
+        try (Stream<String> lines = Files.lines(path)){
+            Object[] arr = lines.toArray();
+
+            for (Object t : arr) {
+                arrList.add(t.toString());
+            }
+        }
+        
+    }
+}
